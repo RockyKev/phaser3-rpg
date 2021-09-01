c0567c4d59cd79c4723440ebec0afcba2dcfd185
rockykev <kev.rocky@gmail.com>
2021-08-31 23:14:51 -0700

    refactored - monster move

--- src/js/game_manager/MonsterModel.js
+++ src/js/game_manager/MonsterModel.js
@@ -2,19 +2,18 @@ import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 import { randomNumber } from './utils.js';
 
 export class MonsterModel {
-    constructor({x, y, gold, spawnerId, frame, health, attack}) {
-
+    constructor({ x, y, gold, spawnerId, frame, health, attack }) {
         this.id = `${spawnerId}-${uuidv4()}`;
-        
+
         this.spawnerId = spawnerId;
 
         this.x = x * 2;
         this.y = y * 2;
         this.gold = gold;
         this.frame = frame;
-        this.health = health; 
-        this.maxHealth = health; 
-        this.attack = attack; 
+        this.health = health;
+        this.maxHealth = health;
+        this.attack = attack;
     }
 
     loseHealth() {
@@ -22,53 +21,30 @@ export class MonsterModel {
         this.health -= 1;
     }
 
-    move() {
-        const positions = [
-          'up',
-          'up-right',
-          'right', 
-          'down-right',
-          'down',
-          'down-left',
-          'left',
-          'up-left'
-        ]
-        
-        const randomPosition = positions[randomNumber(1, 8)];
+    move() {    
         const distance = 64;
- 
-        switch (randomPosition) {
-          case "left":
-            this.x += distance;
-            break;
-          case "right":
-            this.x -= distance;
-            break;
-          case "up":
-            this.y += distance;
-            break;
-          case "down":
-            this.y -= distance;
-            break;
-          case "up-right":
-            this.x += distance;
-            this.y += distance;
-            break;
-          case "down-right":
-            this.x += distance;
-            this.y -= distance;
-            break;
-          case "up-left":
-            this.x -= distance;
-            this.y += distance;
-            break;
-          case "down-right":
-            this.x -= distance;
-            this.y -= distance;
-            break;
-          default:
-            break;
-        }
-  
+
+        // object literal version
+        const directionalMovement = {
+          'up': {x: 0, y: distance},
+          "up-right": {x: distance, y: distance},
+          'right': {x: distance, y: 0},
+          "down-right": {x: distance, y: -distance},
+          'down': {x: 0, y: -distance},
+          "down-left": {x: -distance, y: -distance},
+          'left': {x: -distance, y: 0},
+          "up-left": {x: -distance, y: distance}
+        };
+      
+        // https://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
+        // const randomRirections = directions[randomNumber(1, 8)];
+        var keys = Object.keys(directionalMovement);
+
+        let move = { x: 0, y: 0 };
+        move = directionalMovement[keys[ keys.length * Math.random() << 0 ]]; 
+
+        this.x += move.x;
+        this.y += move.y;
+        
     }
 }
